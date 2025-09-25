e3bff0efcec3b5e07af6fb086cd45bbb3f565bd6
seongha0510 <kingsh0510@tukorea.ac.kr>
2025-09-25 17:29:00 +0900

    배경 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -25,3 +25,12 @@ def handle_events():
                 dir -= 1
             elif event.key == SDLK_LEFT:
                 dir += 1
+
+running = True
+x = 800 // 2
+frame = 0
+dir = 0
+
+while running:
+    clear_canvas()
+    grass.draw(400,30)
