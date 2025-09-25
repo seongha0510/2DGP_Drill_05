d60a0a3bfcf5137a260a06dc9506612cd13d539a
seongha0510 <kingsh0510@tukorea.ac.kr>
2025-09-25 17:26:42 +0900

    QUIT, KEYDOWN, RIGHT 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,5 +1,18 @@
 from pico2d import *
 
 open_canvas()
-background = load_image('TUK_GROUND.png')
+grass = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+
+def handle_events():
+    global running, dir
+
+    global x
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
