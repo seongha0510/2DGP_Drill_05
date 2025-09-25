9dde0d52ebca6d8b1844638597f711c8ee26c82a
seongha0510 <kingsh0510@tukorea.ac.kr>
2025-09-25 17:40:48 +0900

    while running 수정

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -33,14 +33,14 @@ def handle_events():
                 dir_y += 1
 
 running = True
-x = 800 // 2
+x = 400, 90
 frame = 0
-dir = 0
+dir_x, dir_y = 0,0
 
 while running:
     clear_canvas()
     grass.draw(400,300,800,600)
-    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100, 100, 100, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
