1a7b1ac98b288925a7f31c46bc980338680d5c29
seongha0203 <kingsh0510@tukorea.ac.kr>
2025-09-25 15:13:04 +0900

    esc 누르면 닫아지는 코드 실습

--- character_runs_esc.py
+++ character_runs_esc.py
@@ -5,12 +5,19 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 
-# fill here
+running = True
 
 
 
 def handle_events():
-    # fill here
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
     pass
 
 
@@ -22,7 +29,9 @@ for x in range(0, 800, 5):
     character.clip_draw(frame * 100, 100, 100, 100, x, 90)
     update_canvas()
 
-    # fill here
+    handle_events()
+    if not running:
+        break
 
 
 
